f699fcb6e0c5038d7d2a5739a908fc345832db6e
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-13 20:58:11 +0530

    Fetch Product from DB

--- krayabackend/server.js
+++ krayabackend/server.js
@@ -2,22 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-import products from './data/products.js';
 import ConnectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js';
 
 ConnectDB();
 
 app.get('/', (req, res) => {
     res.send("Api is running");
 })
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-})
+app.use('/api/products', productRoutes);
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} on port no :- ${PORT}`);
