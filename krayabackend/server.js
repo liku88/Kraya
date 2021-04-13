eb58743a3ef874411b68d4e5665b01afd80a4adc
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-13 21:18:52 +0530

    Custom Error Handling

--- krayabackend/server.js
+++ krayabackend/server.js
@@ -4,13 +4,18 @@ dotenv.config();
 const app = express();
 import ConnectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 ConnectDB();
 
 app.get('/', (req, res) => {
     res.send("Api is running");
 })
 app.use('/api/products', productRoutes);
+
+
+app.use(notFound)
+
+app.use(errorHandler)
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} on port no :- ${PORT}`);
