66eb8f638a8821d273289d1596d663930809a983
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-16 14:30:17 +0530

    User Authentication Added

--- krayabackend/server.js
+++ krayabackend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 const app = express();
 import ConnectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 ConnectDB();
@@ -13,7 +14,7 @@ app.get('/', (req, res) => {
     res.send("Api is running");
 })
 app.use('/api/products', productRoutes);
-
+app.use('/api/users', userRoutes);
 
 app.use(notFound)
 
