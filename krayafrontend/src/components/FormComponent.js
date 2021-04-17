2449d3608e82d338e66e002a0628f832a1f168fe
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-17 11:43:41 +0530

    Login-Screen Added

---
+++ krayafrontend/src/components/FormComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Form, Button, Row, Col, Container } from 'react-bootstrap'
+const FormComponent = ({ children }) => {
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col xs={12} md={16}>
+                    {children}
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default FormComponent
