a8c2d1ec34a90550354105250ab9beb5bf0de5f8
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-18 15:47:04 +0530

    Cart Reducer Added

--- krayafrontend/src/store.js
+++ krayafrontend/src/store.js
@@ -19,9 +19,12 @@ const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse
 
 const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromLocalStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const intialState = {
-    cart: { cartItems: cartItemsFromLocalStorage },
-    userLogin: { userInfo: userInfoFromLocalStorage }
+    cart: { cartItems: cartItemsFromLocalStorage, shippingAddress: shippingAddressFromLocalStorage },
+    userLogin: { userInfo: userInfoFromLocalStorage },
+
 };
 const middleware = [thunk];
 const store = createStore(reducer, intialState, composeWithDevTools(applyMiddleware(...middleware)));
