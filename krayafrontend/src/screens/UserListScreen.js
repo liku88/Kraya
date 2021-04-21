a8096da5d73dccde6de30c0616dd30ae2afcd611
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-21 15:56:49 +0530

    User-List Ui , Security Bug Added and Fixed Respectively

--- krayafrontend/src/screens/UserListScreen.js
+++ krayafrontend/src/screens/UserListScreen.js
@@ -4,16 +4,67 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import ErrorMessage from '../components/ErrorMessage'
 import Loader from "../components/Loader"
-import { userList } from '../actions/userActions'
+import { listUsers } from '../actions/userActions'
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
     const dispatch = useDispatch()
     const userList = useSelector(state => state.userList);
     const { loading, error, users } = userList
-    return (
-        <div>
 
-        </div>
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin
+
+    useEffect(() => {
+        if (userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            history.push('/login')
+        }
+    }, [dispatch, history])
+    const deleteHandler = (id) => {
+        console.log('delete')
+    }
+
+    return (
+        <>
+            <h1>Users</h1>
+            {loading ? <Loader /> : error ? <ErrorMessage variant="danger">{error}</ErrorMessage> :
+                (
+                    <Table striped bordered hover responsive className='table-sm'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>EMAIL</th>
+                                <th>ADMIN</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map(user => (
+                                <tr key={user._id}>
+                                    <td>{user._id}</td>
+                                    <td>{user.name}</td>
+                                    <td><a href={`mailto:${user.email}`}>{user.email}</a></td>
+                                    <td>{user.isAdmin ? (<i className='fas fa-check' style={{ color: 'green' }}></i>) : (
+                                        <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                    )}</td>
+                                    <td>
+                                        <LinkContainer to={`/user/${user._id}/edit`}>
+                                            <Button variant='light' className='btn-sm'>
+                                                <i className='fas fa-edit'></i>
+                                            </Button>
+                                        </LinkContainer>
+                                        <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
+                                            <i className='fas fa-trash'></i>
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                )}
+        </>
     )
 }
 
