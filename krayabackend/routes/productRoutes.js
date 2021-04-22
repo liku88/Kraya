eb952b401c122017162b965d837ad3fa0b1076f4
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-22 10:37:04 +0530

    Update Product (Backend) Added

--- krayabackend/routes/productRoutes.js
+++ krayabackend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { admin, protect } from '../middleware/authMiddleware.js'
-import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 
 
