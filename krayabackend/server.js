fdb80b9c0feaad01ff4706196cd70afdf13190bc
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-22 18:35:06 +0530

    Multer Implementation

--- krayabackend/server.js
+++ krayabackend/server.js
@@ -6,8 +6,9 @@ import ConnectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-
+import path from 'path'
 
 ConnectDB();
 app.use(express.json())
@@ -18,9 +19,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
