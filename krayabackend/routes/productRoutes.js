a060074f91bf4d4b8c78f938962beef5c5ee863c
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-23 19:12:10 +0530

    Get Top Rated Products from db

--- krayabackend/routes/productRoutes.js
+++ krayabackend/routes/productRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import { admin, protect } from '../middleware/authMiddleware.js'
-import { getProducts, getProductById, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct, createProductReview, getTopProducts } from '../controllers/productController.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
-
+router.get('/top', getTopProducts)
 
 
 export default router;
