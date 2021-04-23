5ed231fa7f75163ff840ff16e641287fbfe3d589
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-23 09:05:45 +0530

    Product Reviews and Ratings (Backend) Added

--- krayabackend/routes/productRoutes.js
+++ krayabackend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { admin, protect } from '../middleware/authMiddleware.js'
-import { getProducts, getProductById, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productController.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 
