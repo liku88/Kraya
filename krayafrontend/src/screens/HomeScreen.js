8b95ccd5353b85d974689b74662f46eb095cb83f
Mangaldeep Das <rockonliku88@gmail.com>
2021-04-23 19:05:16 +0530

    Pagination (frontend)  Added

--- krayafrontend/src/screens/HomeScreen.js
+++ krayafrontend/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import ErrorMessage from '../components/ErrorMessage'
+import Paginate from '../components/Paginate'
 // import axios from 'axios'
 // import products from "../products";
 
@@ -16,7 +17,7 @@ const HomeScreen = ({ match }) => {
 
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList);
-    const { loading, products, error } = productList
+    const { loading, products, error, page, pages } = productList
 
     useEffect(() => {
 
@@ -40,14 +41,20 @@ const HomeScreen = ({ match }) => {
                 ))}
             </Row> */}
             <h1>Latest Products</h1>
-            {loading ? <Loader /> : error ? <ErrorMessage variant='danger'>{error}</ErrorMessage> :
-                <Row>
-                    {products.map((product) => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))}
-                </Row>}
+            {loading ? (<Loader />) : error ? (<ErrorMessage variant='danger'>{error}</ErrorMessage>) :
+                (
+                    <>
+                        <Row>
+                            {products.map((product) => (
+                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={product} />
+                                </Col>
+                            ))}
+                        </Row>
+                        <Paginate pages={pages} page={page} keyword={keyword ? keyword : ''} />
+
+                    </>
+                )}
 
         </>
     )
--- krayafrontend/src/screens/ProductListScreen.js
+++ krayafrontend/src/screens/ProductListScreen.js
@@ -6,11 +6,13 @@ import ErrorMessage from '../components/ErrorMessage'
 import Loader from "../components/Loader"
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from "../constants/productConstants"
+import Paginate from "../components/Paginate"
 
 const ProductListScreen = ({ history, match }) => {
+    const pageNumber = match.params.pageNumber || 1
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList);
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     const productDelete = useSelector(state => state.productDelete);
     const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
@@ -33,9 +35,9 @@ const ProductListScreen = ({ history, match }) => {
         if (successCreate) {
             history.push(`/admin/product/${createdProduct._id}/edit`)
         } else {
-            dispatch(listProducts())
+            dispatch(listProducts('', pageNumber))
         }
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure')) {
@@ -65,39 +67,42 @@ const ProductListScreen = ({ history, match }) => {
             {errorCreate && <ErrorMessage variant='danger' >{errorCreate}</ErrorMessage>}
             {loading ? <Loader /> : error ? <ErrorMessage variant="danger">{error}</ErrorMessage> :
                 (
-                    <Table striped bordered hover responsive className='table-sm'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>NAME</th>
-                                <th>PRICE</th>
-                                <th>CATEGORY</th>
-                                <th>BRANDS</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map(product => (
-                                <tr key={product._id}>
-                                    <td>{product._id}</td>
-                                    <td>{product.name}</td>
-                                    <td>{product.price}</td>
-                                    <td>{product.category}</td>
-                                    <td>{product.brand}</td>
-                                    <td>
-                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                            <Button variant='light' className='btn-sm'>
-                                                <i className='fas fa-edit'></i>
-                                            </Button>
-                                        </LinkContainer>
-                                        <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
-                                            <i className='fas fa-trash'></i>
-                                        </Button>
-                                    </td>
+                    <>
+                        <Table striped bordered hover responsive className='table-sm'>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>NAME</th>
+                                    <th>PRICE</th>
+                                    <th>CATEGORY</th>
+                                    <th>BRANDS</th>
+                                    <th></th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </Table>
+                            </thead>
+                            <tbody>
+                                {products.map(product => (
+                                    <tr key={product._id}>
+                                        <td>{product._id}</td>
+                                        <td>{product.name}</td>
+                                        <td>{product.price}</td>
+                                        <td>{product.category}</td>
+                                        <td>{product.brand}</td>
+                                        <td>
+                                            <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                                <Button variant='light' className='btn-sm'>
+                                                    <i className='fas fa-edit'></i>
+                                                </Button>
+                                            </LinkContainer>
+                                            <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
+                                                <i className='fas fa-trash'></i>
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                        <Paginate pages={pages} page={page} isAdmin={true} />
+                    </>
                 )}
         </>
     )
